6774d249a7223934aee74fd00bf35eefe6fcccf6
Rohanpandey914064 <rohanrock4019@gmail.com>
2026-02-23 10:00:58 +0530

    add first 3 section

---
+++ 2_build_in_data_types.dart
@@ -0,0 +1,15 @@
+void main(List<String> arguments){
+  int number = 18;
+  //var num = 19;
+
+  double percentage = 89.67;
+  
+  bool isEven = true;
+
+  String name = 'Rohan pandey';
+
+  print(number);
+  print(name);
+  print(percentage);
+  print(isEven);
+}
---
+++ 3_string_and_string_interpolation.dart
@@ -0,0 +1,21 @@
+void main(){
+  var isCool = true;
+  int x = 2;
+  "rohan";
+  4.5;
+
+  String s1 = 'Rohan Pandey';
+  String s2 = "Rohan Pandey";
+  String s3 = 'Rohan\'s Pandey';
+  String s4 = "Rohan's Pandey";
+
+  String name = "Rohan Pandey";
+  print("My name is $name");
+  print("the number of character in my name is ${name.length}");
+
+  int l = 20;
+	int b = 10;
+
+	print("The sum of $l and $b is ${l + b}");
+	print("The area of rectangle with length $l and breadth $b is ${l * b}");
+}
