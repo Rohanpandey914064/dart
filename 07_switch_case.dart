c415a8c34da67011a2b6c3bc720c9828597f956f
Rohanpandey914064 <rohanrock4019@gmail.com>
2026-02-23 15:17:01 +0530

    add next 3 section

---
+++ 07_switch_case.dart
@@ -0,0 +1,25 @@
+void main() {
+  String grade = 'A';
+
+  switch(grade) {
+
+    case 'A' : 
+      print("ye to topper hai.");
+      break;
+
+    case 'B':
+			print("Very Good !");
+			break;
+
+		case 'C':
+			print("Good enough. But work hard");
+			break;
+
+		case 'F':
+			print("You have failed");
+			break;
+
+    default:
+      print("Me kuchh nhi bol sakta.");
+  }
+}
