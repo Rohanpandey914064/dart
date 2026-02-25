bb22043b5b5823bf5bb069d6462b124d567e0866
Rohanpandey914064 <rohanrock4019@gmail.com>
2026-02-25 09:16:37 +0530

    Add files via upload

---
+++ 08_for_loop.dart
@@ -0,0 +1,11 @@
+void main() {
+  for(int i=0;i<10;i++){
+    print(i);
+  }
+
+  List planetList = ["Mercury","Mars","Venus","Earth"];
+
+  for(String planet in planetList){
+    print(planet);
+  }
+}
